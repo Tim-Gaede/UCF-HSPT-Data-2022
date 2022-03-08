306f41013b9f8c1036a176462500fdb635a8455e
Timothy Gaede <TimGaede@gmail.com>
2022-03-07 23:39:41 -0500

    Delete krypton.py

--- krypton.py
+++
@@ -1,8 +0,0 @@
-# Read in how many elements we need to help Superman identify
-elements = int(input())
-for t in range(elements):
-    # Read in the value of the element
-    value = int(input())
-    atomic_value = value**2;
-    # Print the square of the element so that Superman can look it up on the periodic table
-    print(atomic_value)
